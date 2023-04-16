f2601394d03942977b8892c4dd0c75bbbd753baa
maximsmol <maximsmol@gmail.com>
2023-04-15 18:28:42 -0700

    initial commit
    Signed-off-by: maximsmol <maximsmol@gmail.com>

---
+++ README.md
@@ -0,0 +1,41 @@
+# Maelstrom Challenges in Python
+
+## Challenges
+
+See https://fly.io/dist-sys/ and https://github.com/jepsen-io/maelstrom
+
+## Python Framework
+
+[`/api`](./api/) provides a Python 3.11 framework for solving the challenges
+
+1. Add the repository root to the PYTHONPATH (`sys.path`)
+2. Inherit the `NodeBase` class and provide handler methods
+3. Instantiate the handler class and call `.run` (in an async context)
+
+See [`/echo/main.py`](./echo/main.py) for an example
+
+## Optional Tooling
+
+- [Conda (miniconda)](https://docs.conda.io/) for managing python installationsen/latest/miniconda.html
+- [direnv](https://direnv.net/) for loading a Conda environment automatically
+- [Just](https://github.com/casey/just/) for running commands
+- [`dev-requirements.txt`](./dev-requirements.txt) lists formatters and linters recommended for development
+
+## Copying/License
+
+This software is quad-licensed and downstream users are free to use any of the provided licenses.
+
+**Available licenses:**
+
+| Name           | Requirements | [OSI][1] Approved      | Notes                                                                  |
+| -------------- | ------------ | ---------------------- | ---------------------------------------------------------------------- |
+| [MIT][2]       | Attribution  | :white_check_mark: Yes | Most commonly recognized and understood                                |
+| [BSD0][3]      | None         | :white_check_mark: Yes | Unencumbered license allowed at Google                                 |
+| [CC0][4]       | None         | :x: No                 | Preferred way of dedicating software to the public domain              |
+| [Unlicense][5] | None         | :white_check_mark: Yes | OSI approved public domain dedication. **Questionable legal standing** |
+
+[1]: https://opensource.org/
+[2]: ./MIT.LICENSE
+[3]: ./BSD0.LICENSE
+[4]: ./COPYING
+[5]: ./UNLICENSE
---
+++ api/__init__.py
@@ -0,0 +1,178 @@
+import asyncio
+from dataclasses import dataclass
+import dataclasses
+import json
+import sys
+import traceback
+from typing import Any, Generic, Literal, TypeAlias, TypeVar
+
+from api.errors import MaelstormError, MaelstormErrorCode, NotSupportedError
+
+_og_print = print
+
+
+def print(*args, **kwargs):
+    _og_print(*args, **kwargs, file=sys.stderr)
+
+
+async def setup_stdio():
+    # alex_noname CC BY-SA 4.0 https://stackoverflow.com/a/64317899
+    # - Cosmetic changes were made to code layout and variable naming
+    loop = asyncio.get_running_loop()
+
+    stdin = asyncio.StreamReader()
+    protocol = asyncio.StreamReaderProtocol(stdin)
+    await loop.connect_read_pipe(lambda: protocol, sys.stdin)
+
+    w_transport, w_protocol = await loop.connect_write_pipe(
+        asyncio.streams.FlowControlMixin, sys.stdout
+    )
+    stdout = asyncio.StreamWriter(w_transport, w_protocol, stdin, loop)
+
+    return stdin, stdout
+
+
+@dataclass(kw_only=True)
+class Payload:
+    type: str
+    msg_id: int | None = None
+    in_reply_to: int | None = None
+
+
+@dataclass(kw_only=True)
+class InitPayload(Payload):
+    type: Literal["init"] = "init"
+    node_id: str
+    node_ids: list[str]
+
+
+@dataclass(kw_only=True)
+class InitOkPayload(Payload):
+    type: Literal["init_ok"] = "init_ok"
+
+
+@dataclass(kw_only=True)
+class ErrorPayload(Payload):
+    type: Literal["error"] = "error"
+    code: int
+    text: str | None = None
+
+
+PayloadT = TypeVar("PayloadT", bound=Payload)
+
+
+@dataclass(frozen=True, kw_only=True)
+class Message(Generic[PayloadT]):
+    id: int | None = None
+    src: str
+    dest: str
+    body: PayloadT
+
+
+Reply: TypeAlias = tuple[str, PayloadT] | None
+
+# Workload: echo
+
+
+@dataclass(kw_only=True)
+class EchoPayload(Payload):
+    type: Literal["echo"] = "echo"
+    echo: Any
+
+
+@dataclass(kw_only=True)
+class EchoOkPayload(Payload):
+    type: Literal["echo_ok"] = "echo_ok"
+    echo: Any
+
+
+# Node base
+
+
+class NodeBase:
+    stdin: asyncio.StreamReader
+    stdout: asyncio.StreamWriter
+    running: bool
+
+    node_id: str
+    node_ids: list[str]
+
+    async def run(self):
+        print("Setting up I/O")
+        self.stdin, self.stdout = await setup_stdio()
+
+        running = True
+
+        print("Starting main loop")
+        while running:
+            try:
+                line = await self.stdin.readline()
+
+                data: Any
+                try:
+                    data = json.loads(line)
+                except json.JSONDecodeError:
+                    print("Failed to decode message:", line)
+                    print(traceback.format_exc())
+                    continue
+
+                try:
+                    body = data["body"]
+                    del data["body"]
+
+                    type_ = body["type"]
+
+                    reply: Reply = None
+                    if type_ == "init":
+                        self.node_id = body["node_id"]
+                        self.node_ids = body["node_ids"]
+
+                        reply = await self.msg_init(
+                            Message(**data, body=InitPayload(**body))
+                        )
+                    elif type_ == "error":
+                        print(
+                            f"!!! Error ({body['code']})"
+                            + (f": {body['text']}" if body["text"] is not None else "")
+                        )
+                    elif type_ == "echo":
+                        reply = await self.msg_echo(
+                            Message(**data, body=EchoPayload(**body))
+                        )
+                    else:
+                        raise NotSupportedError(f"unknown message type: {type_}")
+
+                    if reply is not None:
+                        reply_dest, reply_body = reply
+                        reply_body.in_reply_to = body["msg_id"]
+                        await self.send(reply_dest, reply_body)
+                except MaelstormError as err:
+                    await self.error(data["dest"], err.code, err.msg)
+            except Exception:
+                print(traceback.format_exc())
+                continue
+
+    async def send(self, dest: str, body: Payload):
+        if not dest[0] == "c" and dest not in self.node_ids:
+            raise ValueError("unknown destination:", dest)
+
+        data = {
+            "src": self.node_id,
+            "dest": dest,
+            "body": {
+                k: v for k, v in dataclasses.asdict(body).items() if v is not None
+            },
+        }
+
+        x = json.dumps(data) + "\n"
+        self.stdout.write(x.encode())
+        await self.stdout.drain()
+
+    async def error(self, dest: str, code: MaelstormErrorCode, text: str | None = None):
+        await self.send(dest, ErrorPayload(code=code.value, text=text))
+
+    async def msg_init(self, msg: Message[InitPayload]) -> Reply[InitOkPayload]:
+        raise NotSupportedError()
+
+    async def msg_echo(self, msg: Message[EchoPayload]) -> Reply[EchoOkPayload]:
+        raise NotSupportedError()
